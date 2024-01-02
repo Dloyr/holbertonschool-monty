f3dac39694bf50ec36c2540400081eea7de6205b
Dloyr <d.loyer59870@gmail.com>
2024-01-02 14:50:59 +0100

    remove the condition if node = NULL

--- pall.c
+++ pall.c
@@ -10,12 +10,6 @@ void pall(stack_t **stack, unsigned int line_number)
 	stack_t *node = *stack;
 	(void)line_number;
 
-	if (node == NULL)
-	{
-		fprintf(stderr, "L%d: can't pall, stack empty\n", line_number);
-		EXIT_FAILURE;
-	}
-
 	while (node != NULL)
 	{
 		printf("%d\n", node->n);
