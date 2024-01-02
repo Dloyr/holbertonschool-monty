b35209b94d3ce04472875c296690ce52d11c3bb3
Dloyr <d.loyer59870@gmail.com>
2024-01-02 12:27:54 +0100

    print all values on the stack

---
+++ pall.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "monty.h"
+#include <stdlib.h>
+
+/**
+ * pall - prints all the values on the stack
+ * @stack: pointer to the top of the stack
+ * @line_number: line number in the file
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *node = *stack;
+	(void)line_number;
+
+	if (node == NULL)
+	{
+		fprintf(stderr, "L%d: can't pall, stack empty\n", line_number);
+		EXIT_FAILURE;
+	}
+
+	while (node != NULL)
+	{
+		printf("%d\n", node->n);
+		node = node->next;
+	}
+}
