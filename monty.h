0d91dad2483dcb7ba1d43d6648d3cbf216581b8f
Dloyr <d.loyer59870@gmail.com>
2024-01-02 14:23:52 +0100

    add prototype of function pop

--- monty.h
+++ monty.h
@@ -33,5 +33,6 @@ typedef struct instruction_s
 
 void pall(stack_t **stack, unsigned int line_number);
 void push(stack_t **stack, unsigned int line_number);
+void pop(stack_t **stack, unsigned int line_number);
 
 #endif
